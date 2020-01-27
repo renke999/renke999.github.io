3f411c0e963a14561987d204da3e21b9dc219041
renke <446917139@qq.com>
2020-01-27 17:10:41 +0800

    Site updated: 2020-01-27 17:10:37

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","00b1f7992228e24bb1a662fb78a5e426"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
